2c9675fc99fc1fa2408fcedc8c328b0eaa34f439
kipngeno kevin <kipngenokevin254@gmail.com>
2023-03-21 16:48:00 +0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,245 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="css/bootstrap-reboot.min.css"/>
+    <link rel="stylesheet" href="css/index.css"/>
+    <!--<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>-->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- CSS FONTS FROM GOOGLE -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@100;200;300;400;600;700;800;900&display=swap" rel="stylesheet">
+
+    <title>Fashion Finds Online Store</title>
+</head>
+<body>
+    <header>
+        <div class="logo">
+            <img src="img/logo.webp" alt="Logo"/>
+
+        </div>
+        <nav class="nav">
+            <span class="nav-link">HOME</span>
+            <div class="dropdown">
+                <span class="nav-link">BAGS</span>
+                <div class="dropdown-content">
+                    <span class="nav-drop"></span>
+                    <span class="nav-drop"></span>
+                    <span class="nav-drop"></span>
+                </div>
+            </div>
+            <div class="dropdown">
+                <span class="nav-link">JEWELRY</span>
+                <div class="dropdown-content">
+                    <!-- Sample content 1 -->
+                    <div class="nav-drop">
+                        <span class="brand_name">Botega</span>
+                        <div class="child_categories">
+                            <span class="child_category">Botega Jewelry</span>
+                            <span class="child_category">Botega Jewelry</span>
+                            <span class="child_category">Botega Jewelry</span>
+                            <span class="child_category">Botega Jewelry</span>
+                            <span class="child_category">Botega Jewelry</span>
+                        </div>   
+                    </div>
+
+                    <!-- Sample Content -->
+                     <!-- Sample content 1 -->
+                     <div class="nav-drop">
+                        <span class="brand_name">Botega</span>
+                        <div class="child_categories">
+                            <span class="child_category">Botega Jewelry</span>
+                            <span class="child_category">Botega Jewelry</span>
+                            <span class="child_category">Botega Jewelry</span>
+                            <span class="child_category">Botega Jewelry</span>
+                            <span class="child_category">Botega Jewelry</span>
+                        </div>   
+                    </div>
+
+                    <!-- Sample Content -->
+                     <!-- Sample content 1 -->
+                     <div class="nav-drop">
+                        <span class="brand_name">Botega</span>
+                        <div class="child_categories">
+                            <span class="child_category">Botega Jewelry</span>
+                            <span class="child_category">Botega Jewelry</span>
+                            <span class="child_category">Botega Jewelry</span>
+                            <span class="child_category">Botega Jewelry</span>
+                            <span class="child_category">Botega Jewelry</span>
+                        </div>   
+                    </div>
+
+                    <!-- Sample Content -->
+                     <!-- Sample content 1 -->
+                     <div class="nav-drop">
+                        <span class="brand_name">Botega</span>
+                        <div class="child_categories">
+                            <span class="child_category">Botega Jewelry</span>
+                            <span class="child_category">Botega Jewelry</span>
+                            <span class="child_category">Botega Jewelry</span>
+                            <span class="child_category">Botega Jewelry</span>
+                            <span class="child_category">Botega Jewelry</span>
+                        </div>   
+                    </div>
+
+                    <!-- Sample Content -->
+                     <!-- Sample content 1 -->
+                     <div class="nav-drop">
+                        <span class="brand_name">Botega</span>
+                        <div class="child_categories">
+                            <span class="child_category">Botega Jewelry</span>
+                            <span class="child_category">Botega Jewelry</span>
+                            <span class="child_category">Botega Jewelry</span>
+                            <span class="child_category">Botega Jewelry</span>
+                            <span class="child_category">Botega Jewelry</span>
+                        </div>   
+                    </div>
+
+                    <!-- Sample Content -->
+                     <!-- Sample content 1 -->
+                     <div class="nav-drop">
+                        <span class="brand_name">Botega</span>
+                        <div class="child_categories">
+                            <span class="child_category">Botega Jewelry</span>
+                            <span class="child_category">Botega Jewelry</span>
+                            <span class="child_category">Botega Jewelry</span>
+                            <span class="child_category">Botega Jewelry</span>
+                            <span class="child_category">Botega Jewelry</span>
+                        </div>   
+                    </div>
+
+                    <!-- Sample Content -->
+                     <!-- Sample content 1 -->
+                     <div class="nav-drop">
+                        <span class="brand_name">Botega</span>
+                        <div class="child_categories">
+                            <span class="child_category">Botega Jewelry</span>
+                            <span class="child_category">Botega Jewelry</span>
+                            <span class="child_category">Botega Jewelry</span>
+                            <span class="child_category">Botega Jewelry</span>
+                            <span class="child_category">Botega Jewelry</span>
+                        </div>   
+                    </div>
+
+                    <!-- Sample Content -->
+                    <!-- Sample content 1 -->
+                    <div class="nav-drop">
+                        <span class="brand_name">Botega</span>
+                        <div class="child_categories">
+                            <span class="child_category">Botega Jewelry</span>
+                            <span class="child_category">Botega Jewelry</span>
+                            <span class="child_category">Botega Jewelry</span>
+                            <span class="child_category">Botega Jewelry</span>
+                            <span class="child_category">Botega Jewelry</span>
+                        </div>   
+                    </div>
+                    <!-- Sample content 1 -->
+                    <div class="nav-drop">
+                        <span class="brand_name">Botega</span>
+                        <div class="child_categories">
+                            <span class="child_category">Botega Jewelry</span>
+                            <span class="child_category">Botega Jewelry</span>
+                            <span class="child_category">Botega Jewelry</span>
+                            <span class="child_category">Botega Jewelry</span>
+                            <span class="child_category">Botega Jewelry</span>
+                        </div>   
+                    </div>
+                    <!-- Sample content 1 -->
+                    <div class="nav-drop">
+                        <span class="brand_name">Botega</span>
+                        <div class="child_categories">
+                            <span class="child_category">Botega Jewelry</span>
+                            <span class="child_category">Botega Jewelry</span>
+                            <span class="child_category">Botega Jewelry</span>
+                            <span class="child_category">Botega Jewelry</span>
+                            <span class="child_category">Botega Jewelry</span>
+                        </div>   
+                    </div>
+                    <!-- Sample content 1 -->
+                    <div class="nav-drop">
+                        <span class="brand_name">Botega</span>
+                        <div class="child_categories">
+                            <span class="child_category">Botega Jewelry</span>
+                            <span class="child_category">Botega Jewelry</span>
+                            <span class="child_category">Botega Jewelry</span>
+                            <span class="child_category">Botega Jewelry</span>
+                            <span class="child_category">Botega Jewelry</span>
+                        </div>   
+                    </div>
+                    <!-- Sample content 1 -->
+                    <div class="nav-drop">
+                        <span class="brand_name">Botega</span>
+                        <div class="child_categories">
+                            <span class="child_category">Botega Jewelry</span>
+                            <span class="child_category">Botega Jewelry</span>
+                            <span class="child_category">Botega Jewelry</span>
+                            <span class="child_category">Botega Jewelry</span>
+                            <span class="child_category">Botega Jewelry</span>
+                        </div>   
+                    </div>
+                    <!-- Sample content 1 -->
+                    <div class="nav-drop">
+                        <span class="brand_name">Botega</span>
+                        <div class="child_categories">
+                            <span class="child_category">Botega Jewelry</span>
+                            <span class="child_category">Botega Jewelry</span>
+                            <span class="child_category">Botega Jewelry</span>
+                            <span class="child_category">Botega Jewelry</span>
+                            <span class="child_category">Botega Jewelry</span>
+                        </div>   
+                    </div>
+
+                </div>
+            </div>
+            <div class="dropdown">
+                <span class="nav-link">SHOES</span>
+                <div class="dropdown-content">
+                    <span class="nav-drop"></span>
+                    <span class="nav-drop"></span>
+                    <span class="nav-drop"></span>
+                </div>
+            </div>
+            <div class="dropdown">
+                <span class="nav-link">WATCHES</span>
+                <div class="dropdown-content">
+                    <span class="nav-drop"></span>
+                    <span class="nav-drop"></span>
+                    <span class="nav-drop"></span>
+                </div>
+            </div>
+            <div class="icons">
+                <form class="icons-form">
+                    <input type="text" id="fname" name="fname" placeholder="search">
+                    <button class="search-button">SEARCH</button>
+                </form>
+                <div class="cart">                
+                    <img src="img/cart.png" class="cart-icon" width="35" height="35"/>
+                    <span class="cart-counter">1</span>
+                    <div class="cart-items">
+                    <div class="item">
+                        <img src="img/icons8-bag-48.png" alt="product"/>
+                        <span>Chanel Classic Handbag ajdoijoiajfoakpoa kfposkfmfamoai</span>
+                        <span>Price</span>
+                        <img src="img/icons8-trash-can-24.png" width="24">
+                    </div>
+                    <div class="item">
+                        <img src="img/icons8-bag-48.png" alt="product"/>
+                        <span>Chanel Classic Handbag</span>
+                        <span>Price</span>
+                        <img src="img/icons8-trash-can-24.png" width="24">
+                    </div>
+                </div>
+                </div>
+                
+            </div>
+        </nav>
+    </header>
+    
+    
+</body>
+</html>
